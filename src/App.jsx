c071a22e1abff3acb8155242c5b995a5a5b69981
rainingut <keeprainingut@gmail.com>
2024-09-26 23:36:49 +0800

    initial app

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      {/* https://medium.com/@johnnyfang_11536/vite-build-react-38fb56a1fa4e */}
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -25,9 +17,6 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
